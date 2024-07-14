20a618c5a0d1d8f2d020e3bfe9d1580cbe2cd05c
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2024-07-14 13:35:09 +0530

    [Rahul] | Add. Initialize Spring Boot Backend

---
+++ src/main/java/com/einherjar/sisyphus/SisyphusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.einherjar.sisyphus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisyphusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisyphusBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/einherjar/sisyphus/SisyphusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.einherjar.sisyphus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisyphusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
